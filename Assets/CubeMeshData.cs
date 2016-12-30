48607a2cc11ffdf58e92b06f9dffd5336ba01c1c
Piliponful <piliponful@gmail.com>
2016-12-30 22:15:59 +0200

    All functions for mesh data(verts, tris, uvs) was encapsulated in thair own class

---
+++ Assets/CubeMeshData.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CubeMeshData
+{
+    public Vector3Int cubeSize;
+    private BlockScheme cubeScheme;
+    // cube relative position decoupled for ease of use
+    private int x, y, z;
+    // x, y, z size of one single cube decoupled for ease of use
+    private int sizeX, sizeY, sizeZ;
+    // Declare Points one at a time
+    private Vector3 LeftBottomP, LeftUpperP, RightBottomP, RightUpperP, BackleftBottomP, BackLeftUpperP, BackRightBottomP, BackRightUpperP;
+
+    // ========ACTUAL SINGLE CUBE MESH DATA DECLORATION==========
+    public List<Vector3> verts = new List<Vector3>();
+    public List<int> tris = new List<int>();
+    public List<Vector2> uvs = new List<Vector2>();
+    // ======================END==================
+
+    private int triIndexStart;
+    public int triIndexEnd, uvsIterations;
+
+    public CubeMeshData(BlockScheme cubeScheme, Vector3Int cubeSize, int startIndex)
+    {
+        this.cubeScheme = cubeScheme;
+        this.sizeX = cubeSize.x;
+        this.sizeY = cubeSize.y;
+        this.sizeZ = cubeSize.z;
+        this.x = cubeScheme.position.x;
+        this.y = cubeScheme.position.y;
+        this.z = cubeScheme.position.z;
+        this.triIndexStart = startIndex;
+        this.triIndexEnd = startIndex;
+
+        // Front quad
+        LeftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ + sizeZ);
+        LeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ + sizeZ);
+        RightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ + sizeZ);
+        RightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ + sizeZ);
+
+        // Back quad
+        BackleftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ);
+        BackLeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ);
+        BackRightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ);
+        BackRightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ);
+    }
+
+    public void generateAllMeshDataForCube()
+    {
+        generateVerts4Cube();
+        generateTris4Cube();
+        generateUvs4Cube();
+    }
+
+    // Create verts for quad from input of width, height
+    void generateVerts4Cube()
+    {
+        // Add verts(of predefinded points) to list so that thay form a cube
+
+        if (!cubeScheme.neigbors.frontNeighbor)
+            addFrontFaceVerts();
+        if (!cubeScheme.neigbors.backNeighbor)
+            addBackFaceVerts();
+        if (!cubeScheme.neigbors.rightNeighbor)
+            addRightFaceVerts();
+        if (!cubeScheme.neigbors.leftNeighbor)
+            addLeftFaceVerts();
+        if (!cubeScheme.neigbors.topNeighbor)
+            addUpFaceVerts();
+        if (!cubeScheme.neigbors.bottomNeighbor)
+            addBottomFaceVerts();
+    }
+
+    void addFrontFaceVerts()
+    {
+        // North side of a cube
+        verts.AddRange(
+        generateVerts4Quad(RightBottomP, RightUpperP, LeftUpperP, LeftBottomP));
+        this.triIndexEnd++;
+    }
+
+    void addBackFaceVerts()
+    {
+        // South side of a cube
+        verts.AddRange(
+        generateVerts4Quad(BackleftBottomP, BackLeftUpperP, BackRightUpperP, BackRightBottomP));
+        this.triIndexEnd++;
+    }
+
+    void addRightFaceVerts()
+    {
+        // East side of a cube
+        verts.AddRange(
+        generateVerts4Quad(BackRightBottomP, BackRightUpperP, RightUpperP, RightBottomP));
+        this.triIndexEnd++;
+    }
+
+    void addLeftFaceVerts()
+    {
+        // West side of a cube
+        verts.AddRange(
+        generateVerts4Quad(LeftBottomP, LeftUpperP, BackLeftUpperP, BackleftBottomP));
+        this.triIndexEnd++;
+    }
+
+    void addUpFaceVerts()
+    {
+        verts.AddRange(
+        generateVerts4Quad(BackRightUpperP, BackLeftUpperP, LeftUpperP, RightUpperP));
+        this.triIndexEnd++;
+    }
+
+    void addBottomFaceVerts()
+    {
+        verts.AddRange(
+        generateVerts4Quad(BackleftBottomP, BackRightBottomP, RightBottomP, LeftBottomP));
+        this.triIndexEnd++;
+    }
+
+    List<Vector3> generateVerts4Quad(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    {
+        List<Vector3> verts = new List<Vector3>();
+
+        verts.Add(p1);
+        verts.Add(p2);
+        verts.Add(p3);
+        verts.Add(p4);
+
+        return verts;
+    }
+
+    void generateTris4Cube()
+    {
+        // 6 times for each of cube face
+        for (int i = this.triIndexStart; i < this.triIndexEnd; i++)
+        {
+            tris.AddRange(generateTris4Quad(i));
+        }
+    }
+
+    List<int> generateTris4Quad(int iteration)
+    {
+        // Declare list of tris
+        List<int> tris = new List<int>();
+
+        // Adding tris to list
+
+        // on each iteration we use 4 verts to make 2 triangles that make up quad
+        // so with each iteration verts index should increase in 4
+        tris.Add(iteration * 4 + 0);
+        tris.Add(iteration * 4 + 1);
+        tris.Add(iteration * 4 + 2);
+
+        tris.Add(iteration * 4 + 2);
+        tris.Add(iteration * 4 + 3);
+        tris.Add(iteration * 4 + 0);
+
+        return tris;
+    }
+
+    void generateUvs4Cube()
+    {
+        this.uvsIterations -= this.triIndexStart;
+        // one iteration for each side of the cube
+        for (int i = 0; i < this.uvsIterations; i++)
+        {
+            // generating uv for one quad and adding it to the main list
+            generateUvs4Quad();
+        }
+    }
+
+    List<Vector2> generateUvs4Quad()
+    {
+        // Declare list of uvs
+        List<Vector2> uvs = new List<Vector2>();
+
+        // Adding uvs to list
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+
+        return uvs;
+    }
+}
--- Assets/GBAlg2.cs
+++ Assets/GBAlg2.cs
@@ -11,7 +11,7 @@ public class GBAlg2 : MonoBehaviour {
     public int buildingWidth = 5, buildingHeight = 10, buildingLength = 5;
 
     // x, y, z size of one single cube
-    private int cubeSizeX = 1, cubeSizeY = 1, cubeSizeZ = 1;
+    public int cubeSizeX = 1, cubeSizeY = 1, cubeSizeZ = 1;
     // GameObject(meshHolder) size
     public Vector3 cubeSize = new Vector3(6, 6, 6);
 
@@ -33,7 +33,7 @@ public class GBAlg2 : MonoBehaviour {
                 // add mesh to it
                 generateMeshHolder(
             generateRowsOfCubeMeshes(buildingWidth, buildingHeight, buildingLength, cubeSizeX, cubeSizeY, cubeSizeZ),
-            new Vector3(x * buildingWidth + (x * gutter), 0, z * buildingLength + (z * gutter)),
+            new Vector3(x * cubeSizeX * buildingWidth + (x * gutter), 0, z * cubeSizeZ * buildingLength + (z * gutter)),
             cubeSize,
             true, false, Vector3.zero);
             }
@@ -50,7 +50,7 @@ public class GBAlg2 : MonoBehaviour {
 
         Building building = new Building(width, height, length, cubeSize);
 
-        int i = 0;
+        int trisIndexStart = 0;
         // generating rows of cubes
         for (int x = 0; x < building.width; x++)
         {
@@ -60,21 +60,13 @@ public class GBAlg2 : MonoBehaviour {
                 {
                     if (building.buildingScheme[x, y, z] == null)
                         continue;
-                    CubeMeshData Cube = new CubeMeshData(building.buildingScheme[x, y, z], new Vector3Int(cubeSizeX, cubeSizeZ, cubeSizeZ));
+                    CubeMeshData Cube = new CubeMeshData(building.buildingScheme[x, y, z], new Vector3Int(cubeSizeX, cubeSizeZ, cubeSizeZ), trisIndexStart);
+                    Cube.generateAllMeshDataForCube();
+                    trisIndexStart = Cube.triIndexEnd;
 
-                    int times;
-                    // On each iteration generationg single cube (V, T, U)
-                    // and concat with previous value
-                    verts.AddRange(generateVerts4Cube(cubeSizeX, cubeSizeY, cubeSizeZ, x, y, z, building.buildingScheme[x, y, z], out times));
-
-                    for (int j = 0; j < times; j++)
-                    {
-                        tris.AddRange(generateTris4Quad(i));
-                        i++;
-                    }
-
-                    //uv is just static coordinates for every cube of all sizes. Nothing to look at here:)
-                    uvs.AddRange(generateUvs4Cube(times));
+                    verts.AddRange(Cube.verts);
+                    tris.AddRange(Cube.tris);
+                    uvs.AddRange(Cube.uvs);
                 }
             }
         }
@@ -83,147 +75,6 @@ public class GBAlg2 : MonoBehaviour {
         // (V, T, U) and returns completed mesh
         return generateMesh(verts, tris, uvs);
     }
-
-    // Create verts for quad from input of width, height
-    List<Vector3> generateVerts4Cube(float sizeX, float sizeY, float sizeZ, int x, int y, int z, BlockScheme cubeScheme, out int times)
-    {
-        times = 0;
-        // Declare list of verts
-        List<Vector3> verts = new List<Vector3>();
-
-        // Declare Points one at a time
-        Vector3 LeftBottomP, LeftUpperP, RightBottomP, RightUpperP, BackleftBottomP, BackLeftUpperP, BackRightBottomP, BackRightUpperP;
-        
-        // Front quad
-        LeftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ + 0);
-        LeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ + 0);
-        RightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ + 0);
-        RightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ + 0);
-
-        // Back quad
-        BackleftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ + (-sizeZ));
-        BackLeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ + (-sizeZ));
-        BackRightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ + (-sizeZ));
-        BackRightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ + (-sizeZ));
-
-
-        // Add points to list so that thay form a cube
-
-        // North side of cube
-        if (!cubeScheme.neigbors.frontNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(RightBottomP, RightUpperP, LeftUpperP, LeftBottomP));
-            times++;
-        }
-
-        // South
-        if (!cubeScheme.neigbors.backNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(BackleftBottomP, BackLeftUpperP, BackRightUpperP, BackRightBottomP));
-            times++;
-        }
-
-        // West
-        if (!cubeScheme.neigbors.leftNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(LeftBottomP, LeftUpperP, BackLeftUpperP, BackleftBottomP));
-            times++;
-        }
-
-        // East
-        if (!cubeScheme.neigbors.rightNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(BackRightBottomP, BackRightUpperP, RightUpperP, RightBottomP));
-            times++;
-        }
-
-        // Up
-        if (!cubeScheme.neigbors.topNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(BackRightUpperP, BackLeftUpperP, LeftUpperP, RightUpperP));
-            times++;
-        }
-
-        // Bottom
-        if (!cubeScheme.neigbors.bottomNeighbor)
-        {
-            verts.AddRange(
-            generateVerts4Quad(BackleftBottomP, BackRightBottomP, RightBottomP, LeftBottomP));
-            times++;
-        }
-
-
-        return verts;
-    }
-
-    List<Vector3> generateVerts4Quad(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
-    {
-        List<Vector3> verts = new List<Vector3>();
-
-        verts.Add(p1);
-        verts.Add(p2);
-        verts.Add(p3);
-        verts.Add(p4);
-
-        return verts;
-    }
-
-    List<int> generateTris4Quad(int iteration)
-    {
-        // Declare list of tris
-        List<int> tris = new List<int>();
-
-        // Adding tris to list
-
-        // on each iteration we use 4 verts to make 2 triangles that make up quad
-        // so with each iteration verts index should increase in 4
-        tris.Add(iteration * 4 + 0);
-        tris.Add(iteration * 4 + 1);
-        tris.Add(iteration * 4 + 2);
-
-        tris.Add(iteration * 4 + 2);
-        tris.Add(iteration * 4 + 3);
-        tris.Add(iteration * 4 + 0);
-
-        return tris;
-    }
-
-    List<int> generateTris4Cube(int iteration, int times)
-    {
-        List<int> tris = new List<int>();
-
-        // 6 times for each of cube face
-        for (int i = 0; i < times; i++)
-        {
-            tris.AddRange(generateTris4Quad(iteration));
-        }
-
-        return tris;
-    }
-
-    List<Vector2> generateUvs4Cube(int times)
-    {
-        // Declare list of uvs
-        List<Vector2> uvs = new List<Vector2>();
-
-        // one iteration for each side of the cube
-        for (int iCube = 0; iCube < times; iCube++)
-        {
-            // Adding uvs to list
-            uvs.Add(new Vector2(0, 0));
-            uvs.Add(new Vector2(0, 1));
-            uvs.Add(new Vector2(1, 1));
-            uvs.Add(new Vector2(1, 0));
-        }
-
-        return uvs;
-    }
-
     // Create Mesh from intput of verts, tris and uvs lists
     Mesh generateMesh(List<Vector3> verts, List<int> tris, List<Vector2> uvs)
     {
@@ -245,7 +96,10 @@ public class GBAlg2 : MonoBehaviour {
         // Create a game object and add components to it
         GameObject cube = new GameObject("cube");
         if (split)
+        {
             cube.tag = "atom";
+            cube.name = "liquid";
+        }
         else
             cube.tag = "block";
 
@@ -285,179 +139,10 @@ public class GBAlg2 : MonoBehaviour {
             {
                 for (int z = (int)pos.z - ((int)size.z - 1); z < (int)pos.z + 1; z++)
                 {
+                    Debug.Log("lol inside");
                     generateMeshHolder(generateRowsOfCubeMeshes(1, 1, 1, 1, 1, 1), new Vector3(x, y, z), Vector3.one, false, true, pos);
                 }
             }
         }
-    }
-}
-
-public class CubeMeshData
-{
-    public Vector3Int cubeSize;
-    private BlockScheme cubeScheme;
-    // cube relative position decoupled for ease of use
-    private int x, y, z;
-    // x, y, z size of one single cube decoupled for ease of use
-    private int sizeX, sizeY, sizeZ;
-    // Declare Points one at a time
-    private Vector3 LeftBottomP, LeftUpperP, RightBottomP, RightUpperP, BackleftBottomP, BackLeftUpperP, BackRightBottomP, BackRightUpperP;
-    
-    // ========ACTUAL CUBE MESH DATA DECLORATION==========
-    List<Vector3> verts = new List<Vector3>();
-    List<int> tris = new List<int>();
-    List<Vector2> uvs = new List<Vector2>();
-    // ======================END==================
-
-    public CubeMeshData(BlockScheme cubeScheme, Vector3Int cubeSize)
-    {
-        this.cubeScheme = cubeScheme;
-        this.sizeX = cubeSize.x;
-        this.sizeY = cubeSize.y;
-        this.sizeZ = cubeSize.z;
-        this.x = cubeScheme.position.x;
-        this.y = cubeScheme.position.y;
-        this.z = cubeScheme.position.z;
-
-        // Front quad
-        LeftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ + 0);
-        LeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ + 0);
-        RightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ + 0);
-        RightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ + 0);
-
-        // Back quad
-        BackleftBottomP = new Vector3(x * sizeX + 0, y * sizeY + 0, z * sizeZ + (-sizeZ));
-        BackLeftUpperP = new Vector3(x * sizeX + 0, y * sizeY + sizeY, z * sizeZ + (-sizeZ));
-        BackRightBottomP = new Vector3(x * sizeX + sizeX, y * sizeY + 0, z * sizeZ + (-sizeZ));
-        BackRightUpperP = new Vector3(x * sizeX + sizeX, y * sizeY + sizeY, z * sizeZ + (-sizeZ));
-    }
-
-    // Create verts for quad from input of width, height
-    void generateVerts4Cube()
-    {
-        // Add verts(of predefinded points) to list so that thay form a cube
-
-        addFrontFaceVerts();
-        //generateTris4Quad();
-
-        addBackFaceVerts();
-
-        addRightFaceVerts();
-
-        addLeftFaceVerts();
-
-        addUpFaceVerts();
-
-        addBottomFaceVerts();
-    }
-
-    void addFrontFaceVerts()
-    {
-        // North side of a cube
-        verts.AddRange(
-        generateVerts4Quad(RightBottomP, RightUpperP, LeftUpperP, LeftBottomP));
-    }
-
-    void addBackFaceVerts()
-    {
-        // South side of a cube
-        verts.AddRange(
-        generateVerts4Quad(BackleftBottomP, BackLeftUpperP, BackRightUpperP, BackRightBottomP));
-    }
-
-    void addRightFaceVerts()
-    {
-        // East side of a cube
-        verts.AddRange(
-        generateVerts4Quad(BackRightBottomP, BackRightUpperP, RightUpperP, RightBottomP));
-    }
-
-    void addLeftFaceVerts()
-    {
-        // West side of a cube
-        verts.AddRange(
-        generateVerts4Quad(LeftBottomP, LeftUpperP, BackLeftUpperP, BackleftBottomP));
-    }
-
-    void addUpFaceVerts()
-    {
-        verts.AddRange(
-        generateVerts4Quad(BackRightUpperP, BackLeftUpperP, LeftUpperP, RightUpperP));
-    }
-
-    void addBottomFaceVerts()
-    {
-        verts.AddRange(
-        generateVerts4Quad(BackleftBottomP, BackRightBottomP, RightBottomP, LeftBottomP));
-    }
-
-    void addFrontFaceTris()
-    {
-
-    }
-
-    List<Vector3> generateVerts4Quad(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
-    {
-        List<Vector3> verts = new List<Vector3>();
-
-        verts.Add(p1);
-        verts.Add(p2);
-        verts.Add(p3);
-        verts.Add(p4);
-
-        return verts;
-    }
-
-    void generateTris4Cube(int iteration)
-    {
-        // 6 times for each of cube face
-        for (int i = iteration; i < iteration + 6; i++)
-        {
-            tris.AddRange(generateTris4Quad(i));
-        }
-    }
-
-    List<int> generateTris4Quad(int iteration)
-    {
-        // Declare list of tris
-        List<int> tris = new List<int>();
-
-        // Adding tris to list
-
-        // on each iteration we use 4 verts to make 2 triangles that make up quad
-        // so with each iteration verts index should increase in 4
-        tris.Add(iteration * 4 + 0);
-        tris.Add(iteration * 4 + 1);
-        tris.Add(iteration * 4 + 2);
-
-        tris.Add(iteration * 4 + 2);
-        tris.Add(iteration * 4 + 3);
-        tris.Add(iteration * 4 + 0);
-
-        return tris;
-    }
-
-    void generateUvs4Cube()
-    {
-        // one iteration for each side of the cube
-        for (int iCube = 0; iCube < 6; iCube++)
-        {
-            // generating uv for one quad and adding it to the main list
-            generateUvs4Quad();
-        }
-    }
-
-    List<Vector2> generateUvs4Quad()
-    {
-        // Declare list of uvs
-        List<Vector2> uvs = new List<Vector2>();
-
-        // Adding uvs to list
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-
-        return uvs;
     }
 }
